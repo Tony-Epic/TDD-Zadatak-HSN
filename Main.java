e82125761b860aefc5a89d46dea50b40e83506f6
Tony-Epic <seljackicigani@gmail.com>
2020-04-24 21:03:28 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,52 +1,23 @@
-
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		menu();
-	}
-
-	private static void menu() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Unesite neki string : ");
 		String unos=input.nextLine();
+		input.close();
 		
-		System.out.println("Unesite 1 da bi vidjeli duzinu vaseg stringa");
-		System.out.println("Unesite 2 da bi vidjeli sve karaktere na parnim pozicijama");
-		System.out.println("Unesite 3 da bi vidjeli sve karaktere na neparnim pozicijama");
-		System.out.println("Unesite 4 da bi vidjeli broj velikih slova");
-		System.out.println("Unesite 5 da bi vidjeli broj malih slova");
-		System.out.println("Unesite 6 da bi vidjeli sve karaktere koji nisu slova : ");
-		int izbor=input.nextInt();
-		
-		if(izbor == 1)
-		{
-			System.out.println("Duzina vaseg stringa je : "+duzinaStringa(unos));
-		}
-		else if(izbor == 2)
-		{
-			System.out.println("Karakteri na parnim pozicijama su : "+karakteriNaParnimPozicijama(unos));
-		}
-		else if(izbor == 3)
-		{
-			System.out.println("Karakteri na neparnim pozicijama su : "+karakteriNaNeparnimPozicijama(unos));
-		}
-		else if(izbor == 4)
-		{
-			System.out.println("Broj velikih slova je : "+brojVelikihSlova(unos));
-		}
-		else if(izbor == 5)
-		{
-			System.out.println("Broj malih slova je : "+brojMalihSlova(unos));;
-		}
-		else if(izbor == 6)
-		{
-			System.out.println("Karakteri koji nisu slova su : "+karakteriKojiNisuSlova(unos));;
-		}
+		System.out.println("Duzina vaseg stringa je : "+duzinaStringa(unos));
+		System.out.println("Karakteri na parnim pozicijama su : "+karakteriNaParnimPozicijama(unos));
+		System.out.println("Karakteri na neparnim pozicijama su : "+karakteriNaNeparnimPozicijama(unos));
+		System.out.println("Broj velikih slova je : "+brojVelikihSlova(unos));
+		System.out.println("Broj malih slova je : "+brojMalihSlova(unos));
+		System.out.println("Karakteri koji nisu slova su : "+karakteriKojiNisuSlova(unos));;
 		
 	}
 
+	
 	public static int  duzinaStringa(String sc) 
 	{
 		return sc.length();
@@ -103,9 +74,13 @@ public class Main {
 		String karakteri = "";
 		for (int i = 0; i < sc.length(); i++) 
 			if(!Character.isAlphabetic(sc.charAt(i)) && !Character.isWhitespace(sc.charAt(i)))
+			{
+				
 				karakteri += sc.charAt(i);
+			}
 		
 		return karakteri;
 	}
-
+	
 }
+
